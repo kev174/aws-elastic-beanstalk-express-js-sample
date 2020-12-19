a57236c8f31d370b8442545ca76781aa7fbe75ec
kev174 <kev17404@yahoo.co.uk>
2020-12-19 11:17:23 +0000

    Retesting...

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Kevin new AWS Code-Build Pipeline....'));
+app.get('/', (req, res) => res.send('Kevin AWS Code-Build Pipeline: Re-Deployed !!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
